699d76169a5046452109658f67af838ad3f26962
Thomas Sean Dominic Kelly <thomassdk@pm.me>
2021-08-13 18:15:07 +0100

    Build the extension directly from source

--- flake.nix
+++ flake.nix
@@ -27,29 +27,67 @@
       nixpkgsFor = forAllSystems (system:
         import nixpkgs {
           inherit system;
-          overlays = [ self.overlay ];
+          overlays = nixpkgs.lib.attrValues self.overlays;
         });
 
     in {
 
       # A Nixpkgs overlay.
-      overlay = final: prev: {
+      overlays = {
+        firefox-meta-press = final: prev: {
+          firefox-meta-press = with final;
+            wrapFirefox firefox-unwrapped {
+              nixExtensions = [
+                (fetchFirefoxAddon {
+                  name = "meta-press-es"; # Has to be unique!
+                  url =
+                    "https://addons.mozilla.org/firefox/downloads/file/3759736/meta_presses-${version}-an+fx.xpi";
+                  sha256 =
+                    "02glmx9qmra39mpsrsk09wdgx8wgdg45j00ls4gcibmi2n5d4dxi";
+                })
+              ];
+            };
+        };
+        meta-press = final: prev: {
+          meta-press = with final;
+            stdenv.mkDerivation rec {
+              name = "meta-press-${version}";
 
-        firefox-meta-press = with final;
-          wrapFirefox firefox-unwrapped {
-            nixExtensions = [
-              (fetchFirefoxAddon {
-                name = "meta-press-es"; # Has to be unique!
-                url =
-                  "https://addons.mozilla.org/firefox/downloads/file/3759736/meta_presses-${version}-an+fx.xpi";
-                sha256 = "02glmx9qmra39mpsrsk09wdgx8wgdg45j00ls4gcibmi2n5d4dxi";
-              })
-            ];
-          };
+              src = meta-press;
+
+              buildInputs = with final; [ p7zip zip ];
+
+              # The Make file moves the output to the enclosing folder
+              # This makes use of the fact that mv ignores dotfiles
+              buildPhase = ''
+                mkdir .build
+                mv * .build
+                cd .build
+                make
+              '';
+
+              # An xpi is just a renamed zip for firefox extensions
+              installPhase = ''
+                mkdir $out
+                ZIP=$(find ../ -type f -name "*.zip")
+                mv -- "$ZIP" "$(basename -- "$ZIP" .zip).xpi"
+                XPI=$(find ../ -type f -name "*.xpi")
+                mv $XPI $out
+              '';
+
+              meta = {
+                homepage = "https://www.meta-press.es";
+                description =
+                  "Decentralized search engine & automatized press reviews";
+              };
+            };
+        };
       };
 
-      packages = forAllSystems
-        (system: { inherit (nixpkgsFor.${system}) firefox-meta-press; });
+      packages = forAllSystems (system: {
+        inherit (nixpkgsFor.${system}) firefox-meta-press;
+        inherit (nixpkgsFor.${system}) meta-press;
+      });
 
       defaultPackage =
         forAllSystems (system: self.packages.${system}.firefox-meta-press);
