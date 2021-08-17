fc2db7b854965e3e953c02dedb26a9f46f4f4090
Thomas Sean Dominic Kelly <thomassdk@pm.me>
2021-08-17 12:19:55 +0100

    Made revisions based on pull review
    https://github.com/ngi-nix/meta-press/pull/1

--- flake.nix
+++ flake.nix
@@ -5,19 +5,19 @@
   # Nixpkgs / NixOS version to use.
   inputs.nixpkgs.url = "nixpkgs/21.05";
 
-  inputs.meta-press = {
+  inputs.meta-press-src = {
     url = "https://framagit.org/Siltaar/meta-press-ext";
     flake = false;
     submodules = true;
     type = "git";
   };
 
-  outputs = { self, nixpkgs, meta-press }:
+  outputs = { self, nixpkgs, meta-press-src }:
     let
       version = "1.7.7";
 
       # System types to support.
-      supportedSystems = [ "x86_64-linux" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = f:
@@ -27,16 +27,55 @@
       nixpkgsFor = forAllSystems (system:
         import nixpkgs {
           inherit system;
-          overlays = nixpkgs.lib.attrValues self.overlays;
+          overlays = self.overlays;
         });
+
+      meta-press = { src, lib, stdenv, p7zip }:
+        stdenv.mkDerivation {
+          name = "meta-press-${version}";
+
+          src = meta-press-src;
+
+          buildInputs = [ p7zip ];
+
+          # The Make file moves the output to the enclosing folder
+          # This makes use of the fact that mv ignores dotfiles
+          buildPhase = ''
+            mkdir .build
+            mv * .build
+            cd .build
+            make
+          '';
+
+          # An xpi is just a renamed zip for firefox extensions
+          installPhase = ''
+            mkdir -p $out
+            ZIP=$(find ../ -type f -name "*.zip")
+            mv -- "$ZIP" "$(basename -- "$ZIP" .zip).xpi"
+            XPI=$(find ../ -type f -name "*.xpi")
+            mv $XPI $out/firefox_addon.xpi
+          '';
+
+          meta = {
+            homepage = "https://www.meta-press.es";
+            description =
+              "Decentralized search engine & automatized press reviews";
+            license = with lib.licenses; [ mit gpl3 ];
+          };
+        };
 
     in {
 
       # A Nixpkgs overlay.
-      overlays = {
-        firefox-meta-press = final: prev: {
-          firefox-meta-press = with final;
-            wrapFirefox firefox-unwrapped {
+      overlays = [
+        (final: prev: {
+          meta-press = final.callPackage meta-press {};
+        })
+
+        (final: prev: {
+          firefox-meta-press-pinned =
+            let inherit (final) wrapFirefox firefox-unwrapped fetchFirefoxAddon;
+            in wrapFirefox firefox-unwrapped {
               nixExtensions = [
                 (fetchFirefoxAddon {
                   name = "meta-press-es"; # Has to be unique!
@@ -47,46 +86,49 @@
                 })
               ];
             };
-        };
-        meta-press = final: prev: {
-          meta-press = with final;
-            stdenv.mkDerivation rec {
-              name = "meta-press-${version}";
+        })
+        (final: prev: {
+          firefox-meta-press = let
+            inherit (final)
+              lib meta-press stdenv wrapFirefox firefox-unwrapped fetchFirefoxAddon;
+          in wrapFirefox firefox-unwrapped {
+            nixExtensions = [
+              # Waiting for a pull request to be merged into nixpkgs
+              # https://github.com/NixOS/nixpkgs/pull/134427
+              # ((fetchFirefoxAddon {
+              # name = "meta-press-es"; # Has to be unique!
+              # src = "${meta-press.outPath}/firefox_addon.xpi";
+              # }))
+              (stdenv.mkDerivation rec {
+                name = "meta-press-es";
+                extid = "nixos@${name}";
+                passthru = { inherit extid; };
+                nativeBuildInputs = with prev.pkgs; [ coreutils unzip zip jq ];
+                src = "${meta-press.outPath}/firefox_addon.xpi";
+                builder = prev.writeScript "xpibuilder" ''
+                  source $stdenv/setup
 
-              src = meta-press;
+                  header "firefox addon $name into $out"
 
-              buildInputs = with final; [ p7zip zip ];
-
-              # The Make file moves the output to the enclosing folder
-              # This makes use of the fact that mv ignores dotfiles
-              buildPhase = ''
-                mkdir .build
-                mv * .build
-                cd .build
-                make
-              '';
-
-              # An xpi is just a renamed zip for firefox extensions
-              installPhase = ''
-                mkdir $out
-                ZIP=$(find ../ -type f -name "*.zip")
-                mv -- "$ZIP" "$(basename -- "$ZIP" .zip).xpi"
-                XPI=$(find ../ -type f -name "*.xpi")
-                mv $XPI $out
-              '';
-
-              meta = {
-                homepage = "https://www.meta-press.es";
-                description =
-                  "Decentralized search engine & automatized press reviews";
-              };
-            };
-        };
-      };
+                  UUID="${extid}"
+                  mkdir -p "$out/$UUID"
+                  unzip -q ${src} -d "$out/$UUID"
+                  NEW_MANIFEST=$(jq '. + {"applications": { "gecko": { "id": "${extid}" }}, "browser_specific_settings":{"gecko":{"id": "${extid}"}}}' "$out/$UUID/manifest.json")
+                  echo "$NEW_MANIFEST" > "$out/$UUID/manifest.json"
+                  cd "$out/$UUID"
+                  zip -r -q -FS "$out/$UUID.xpi" *
+                  rm -r "$out/$UUID"
+                '';
+              })
+            ];
+          };
+        })
+      ];
 
       packages = forAllSystems (system: {
-        inherit (nixpkgsFor.${system}) firefox-meta-press;
         inherit (nixpkgsFor.${system}) meta-press;
+        inherit (nixpkgsFor.${system}) firefox-meta-press;
+        inherit (nixpkgsFor.${system}) firefox-meta-press-pinned;
       });
 
       defaultPackage =
